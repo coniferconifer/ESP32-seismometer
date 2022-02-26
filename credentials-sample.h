c9c732db7805e226c311c8afc92bf07dc68b232f
coniferconifer <32008307+coniferconifer@users.noreply.github.com>
2022-02-26 15:29:58 +0900

    template of credentials.h

---
+++ credentials-sample.h
@@ -0,0 +1,16 @@
+#define WIFI_SSID "wifistation1"
+#define WIFI_PASS  "xxxxxxxxx"
+
+#define WIFI_SSID1 "wifistation2"
+#define WIFI_PASS1  "yyyyyyyyyy"
+
+#define WIFI_SSID2 "APatworkplace"
+#define WIFI_PASS2  "zzzzzzzzzz"
+
+#define TOKEN "AAAAAAAAAAAAA"
+#define TOKEN1 "AAAAAAAAAAAAA"
+#define TOKEN2 "BBBBBBBBBBBBBB"
+
+#define SERVER "192.168.13.201"
+#define SERVER1 "192.168.13.201"
+#define SERVER2 "192.168.52.24"
