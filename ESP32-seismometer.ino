bdf504860e7fa40ca7f10e6d059cb3b7b496b3c2
coniferconifer <32008307+coniferconifer@users.noreply.github.com>
2022-08-14 18:38:36 +0900

    bug fix for MPU6050 register read out

--- ESP32-seismometer.ino
+++ ESP32-seismometer.ino
@@ -641,7 +641,7 @@ int initMPU6050() {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr, 1, true);
   c = Wire.read();
-  Serial.print("MPU9250 / MPU6050 WHO_AM_I (0x68 / 0x71): ");7
+  Serial.print("MPU9250 / MPU6050 WHO_AM_I (0x71 / 0x68): ");
   Serial.println(c, HEX);
   if (!(c==0x71 || c==0x68)) {
     return -1;
@@ -670,6 +670,11 @@ void getAxAyAz( int16_t *AcX, int16_t *AcY , int16_t *AcZ) {
     *AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
     *AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
     *AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    int16_t  Tmp,GyX,GyY,GyZ;
+    Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+    GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)  
+    GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+    GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
     xSemaphoreGive( semaI2C );  /* give one semaphore */
   }
 }
